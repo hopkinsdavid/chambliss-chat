a218ef369ee322a59ae032c149983f00e1ad0ae8
David Hopkins <davidhopkins998@gmail.com>
2025-10-23 12:42:35 -0400

    Fix: Bind server to 0.0.0.0 for Render deployment

--- server/index.js
+++ server/index.js
@@ -213,6 +213,6 @@ io.on("connection", (socket) => {
     
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => { // <-- Add '0.0.0.0' here
+  console.log(`Server listening on http://0.0.0.0:${PORT}`); 
 });
